1ef4ba6d9a72a5c10975c4195592155f8c888dce
anonymousgroundhog <leapfrogopus@gmail.com>
2025-02-17 13:40:38 -0600

    Adding new script for lab 4

---
+++ lab_4.sh
@@ -0,0 +1,6 @@
+cd /usr/share/nmap/scripts
+git clone https://github.com/scipag/vulscan.git
+cd vulscan/utilities/updater/
+chmod +x updateFiles.sh
+./updateFiles.sh
+git clone https://github.com/vulnersCom/nmap-vulners.git
